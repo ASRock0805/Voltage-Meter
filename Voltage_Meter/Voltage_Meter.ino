74eecb5b83529cc08a83aedd5f91c743bba9c117
Yi-Xuan Wang (王宜璿) <asrock129913961@gmail.com>
2021-11-30 02:34:22 +0800

    Update Voltage_Meter.ino

--- Voltage_Meter/Voltage_Meter.ino
+++ Voltage_Meter/Voltage_Meter.ino
@@ -15,12 +15,12 @@
 #define N 800     // Measurment sampling number for smoothing
 
 /*--- Constants ---*/
-const unsigned long baudSpeed = 115200;           // Sets the data rate in bits per second (baud) for serial data transmission
-const unsigned long period = 1000;                // The value is a number of milliseconds
+const unsigned long baudSpeed = 115200UL;           // Sets the data rate in bits per second (baud) for serial data transmission
+const unsigned long period = 1000UL;                // The value is a number of milliseconds
 
-const byte vIn = 5;                                   // Supply voltage from Arduino
-const byte resBits = 10;                              // Resolution of ADC (10 bits)
-const float vConv = vIn / (pow(2.0, resBits) - 1.0);  // Voltage of ADC level (2^bits)
+const byte vIn = 5;                                             // Supply voltage from Arduino
+const byte resBits = 10;                                        // Resolution of ADC (10 bits)
+const float vConv = (float)(vIn / (pow(2.0, resBits) - 1.0f));  // Voltage of ADC level (2^bits)
 
 /*--- Global Variables ---*/
 unsigned long startTime;    // Start time
@@ -66,11 +66,11 @@ void loop(void) {
 /*--- Functions Definition ---*/
 // Implementation of Analog Voltage Reading
 void getVolt(byte signalPin) {
-  for (unsigned int i = 0; i < N; ++i) {    // Get samples for smooth the value
+  for (unsigned int i = 0UL; i < N; ++i) {    // Get samples for smooth the value
     vOut = vOut + analogRead(signalPin);
-    delay(1);                               // delay in between reads for stability
+    delay(1);                                 // delay in between reads for stability
   }
-  vOut = (vOut * vConv) / N;                // ADC of voltage meter output voltage
+  vOut = (vOut * vConv) / N;                  // ADC of voltage meter output voltage
 
   if (isinf(vOut) || isnan(vOut)) {
     vOut = -1;
